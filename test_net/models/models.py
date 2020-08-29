4628b1b5d233443b948798de19cf700786d2cece
Divyansh <divy9t12@icloud.com>
2020-08-29 10:50:49 +0530

    Quote Test

--- test_net/models/models.py
+++ test_net/models/models.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class test_net(models.Model):
-#     _name = 'test_net.test_net'
-#     _description = 'test_net.test_net'
+class test_net(models.Model):
+    _name = "test_net.test_net"
+    _description = "test_net.test_net"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends("value")
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
