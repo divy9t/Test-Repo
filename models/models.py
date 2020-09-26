ef3123ac6c82f47dd57364b42493ba1d9ce0c339
Divyansh Tripathi <divy9t12@icloud.com>
2020-09-26 15:19:23 +0530

    Test Commit

--- models/models.py
+++ models/models.py
@@ -1,18 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
 
 
-# class test(models.Model):
-#     _name = 'test.test'
-#     _description = 'test.test'
+class test(models.Model):
+    _name = "test.test"
+    _description = "test.test"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    testField = fields.Char()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends("value")
+    def _value_pc(self):
+        for record in self:
+            record.value2 = float(record.value) / 100
+
+            if (
+                record.name
+                and record.value
+                and record.testField
+                and record.value2
+                and record.description
+                and record
+            ):
+                pass
